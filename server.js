369b7130e61d038f4932195f1daa11f35ef1d5c7
huzaifa11 <huzaifano1@hotmail.com>
2019-05-27 13:48:15 -0700

    push for video call

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');  
-const app = express();  
-app.use(express.static(__dirname + '/dist'));  
-app.all('*', (req, res) => {  
-  res.status(200).sendFile(__dirname + '/social-funda/index.html');  
-});  
-app.listen(process.env.PORT || 8080);  
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/socal-funda'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/socal-funda/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
